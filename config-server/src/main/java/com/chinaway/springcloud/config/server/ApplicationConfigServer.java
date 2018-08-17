6458ee495869090ebf12d83e512b061759219cf3
sxl <sxl@chinawaytek.com>
2018-08-17 18:05:54 +0800

    update

--- config-server/src/main/java/com/chinaway/springcloud/config/server/ApplicationConfigServer.java
+++ config-server/src/main/java/com/chinaway/springcloud/config/server/ApplicationConfigServer.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+@ComponentScan("com.chinaway")
 public class ApplicationConfigServer {
  
     public static void main(String[] args) {
