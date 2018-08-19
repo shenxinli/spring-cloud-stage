ef66388549e54fb7e67d2e7d2600954c87f6283b
Administrator <Administrator@USER-20150513LD>
2018-08-19 16:30:23 +0800

    init

--- config-server/src/main/java/com/chinaway/ApplicationConfigServer.java
+++ config-server/src/main/java/com/chinaway/ApplicationConfigServer.java
@@ -1,13 +1,15 @@
-package com.chinaway.springcloud.config.server;
+package com.chinaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class ApplicationConfigServer {
  
     public static void main(String[] args) {
