1f4bd07bbc0195a6346ad2601a4428877cc8135a
sxl <sxl@chinawaytek.com>
2018-08-20 14:22:22 +0800

    fasdf

--- serice-feign/src/main/java/com/chinaway/service/HiService.java
+++ serice-feign/src/main/java/com/chinaway/service/HiService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi", fallback=HiServiceImpl.class)
 public interface HiService {
 
 	@RequestMapping(value = "/hi")
