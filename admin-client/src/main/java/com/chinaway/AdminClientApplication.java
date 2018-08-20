7efdfcd45eb8ed578c4158454ee9e7fee3978a37
sxl <sxl@chinawaytek.com>
2018-08-20 11:29:40 +0800

    init

--- admin-client/src/main/java/com/chinaway/AdminClientApplication.java
+++ admin-client/src/main/java/com/chinaway/AdminClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @RestController
+@PropertySource(value= {"classpath:demo.properties"})
 public class AdminClientApplication {
  
 	@Value("${demo.value}")
