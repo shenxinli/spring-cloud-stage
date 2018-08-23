787164ac829bb253763dbbde72bc476a828ecf96
Administrator <Administrator@USER-20150513LD>
2018-08-23 21:52:28 +0800

    ffgf

--- admin-server/src/main/java/com/chinaway/AdminServerApplication.java
+++ admin-server/src/main/java/com/chinaway/AdminServerApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
+@RefreshScope
 public class AdminServerApplication {
     private static final Logger log = LoggerFactory.getLogger(AdminServerApplication.class);
 
--- service-ribbon/src/main/java/com/chinaway/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/chinaway/ServiceRibbonApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableHystrix
+@RefreshScope
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
