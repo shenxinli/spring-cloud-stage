74ea5c136e1ace4ae094a8d01cfa63da73d41b6f
Administrator <Administrator@USER-20150513LD>
2018-08-23 21:41:41 +0800

    ffg

--- service-hi/src/main/java/com/chinaway/ServiceHiApplication.java
+++ service-hi/src/main/java/com/chinaway/ServiceHiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.chinaway.config.ServiceInfo;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@RefreshScope
 public class ServiceHiApplication {
 	
 	@Autowired
