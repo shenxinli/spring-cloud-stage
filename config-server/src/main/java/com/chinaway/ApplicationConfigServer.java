0313245ccac0b7758c2a497912e3bf1439d8d792
Administrator <Administrator@USER-20150513LD>
2018-08-24 21:56:26 +0800

    dfgsd

--- config-server/src/main/java/com/chinaway/ApplicationConfigServer.java
+++ config-server/src/main/java/com/chinaway/ApplicationConfigServer.java
@@ -3,7 +3,6 @@ package com.chinaway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
